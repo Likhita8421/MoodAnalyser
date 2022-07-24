ad07a9c3c9e7b3b2de173e634cbc36995c735999
Likhita <leenupandagre24@gmail.com>
2022-07-25 02:02:48 +0530

    changes made

--- MoodAnalyser/MoodAnalyser/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-//Mood_Analyser.MoodAnalyser checkMood = new Mood_Analyser.MoodAnalyser( null);
-//checkMood.AnalyseMood();
-
-Mood_Analyser.MoodAnalyserFactory.CreateMoodAnalyze("Mood_Analyser.MoodAnalyser", "MoodAnalyser");
